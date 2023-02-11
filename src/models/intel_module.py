cad8b7b9ed8cb0d2377aaf9835edff6e837cb7de
Kandula Narasimharao <kandulanarasimharao@gmail.com>
2023-02-11 16:52:29 +0000

    removed LBFGS

--- src/models/intel_module.py
+++ src/models/intel_module.py
@@ -38,7 +38,7 @@ class LitResnet(pl.LightningModule):
 
         self.save_hyperparameters(logger=False)
         self.model = timm.create_model(
-            "resnet34", pretrained=True, num_classes=num_classes
+            "resnet18", pretrained=True, num_classes=num_classes
         )
 
         # loss function
