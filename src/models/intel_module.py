84c825ec099f7986bd3a40910d4046d5bda8183a
Kandula Narasimharao <kandulanarasimharao@gmail.com>
2023-02-12 23:56:50 +0000

    changed init

--- src/models/intel_module.py
+++ src/models/intel_module.py
@@ -87,7 +87,7 @@ class LitResnet(pl.LightningModule):
         # by default lightning executes validation step sanity checks before training starts,
         # so we need to make sure val_acc_best doesn't store accuracy from these checks
         #self.val_acc_best.reset()
-        self.test_acc_best.reset()
+        #self.test_acc_best.reset()
         pass
     
     def model_step(self, batch: Any):
